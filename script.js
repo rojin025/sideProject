303b63fb744ce7eae81228b53dedf2bf0e8cc48c
Rojin Bijukchhe <35796647+rojin025@users.noreply.github.com>
2023-12-22 21:16:10 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ if(y) x= true;
 if(x) console.log("I an false");
 
 const interface = 'audio';
+
+  printArea() {
+    const area = this.length * this.width;
+    const [name, color] = this.getNameAndColor();
+    console.log(`The area of the ${name} ${color} is {area}`);
+    return area;
+  }
+}
+
+console.log(`Start`);
+[1, 2, 3].forEach((i) => setTimeout((_) => console.log(i)), 0);
+console.log(`Finish`);
